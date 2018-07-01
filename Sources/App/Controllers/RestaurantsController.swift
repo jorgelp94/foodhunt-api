85565be11911f583e40273f62c65669793c3274b
Jorge Perales <jorge@axented.com>
2018-07-01 01:40:44 -0500

    Add controllers

---
+++ Sources/App/Controllers/RestaurantsController.swift
@@ -0,0 +1,45 @@
+
+import Vapor
+import Fluent
+
+struct RestaurantsController: RouteCollection {
+    func boot(router: Router) throws {
+        let restaurantsRoutes = router.grouped("api", "restaurants")
+        restaurantsRoutes.get(use: getAllHandler)
+        restaurantsRoutes.post(Restaurant.self, use: createHandler)
+        restaurantsRoutes.get(Restaurant.parameter, use: getHandler)
+        restaurantsRoutes.put(Restaurant.parameter, use: updateHandler)
+        restaurantsRoutes.delete(Restaurant.parameter, use: deleteHandler)
+        restaurantsRoutes.get("search", use: searchHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Restaurant]> {
+        return Restaurant.query(on: req).all()
+    }
+    
+    func createHandler(_ req: Request, restaurant: Restaurant) throws -> Future<Restaurant> {
+        return restaurant.save(on: req)
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<Restaurant> {
+        return try req.parameters.next(Restaurant.self)
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<Restaurant> {
+        return try flatMap(to: Restaurant.self, req.parameters.next(Restaurant.self), req.content.decode(Restaurant.self), { (restaurant, updatedRestaurant) in
+            restaurant.name = updatedRestaurant.name
+            return restaurant.save(on: req)
+        })
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Restaurant.self).delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+    
+    func searchHandler(_ req: Request) throws -> Future<[Restaurant]> {
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return Restaurant.query(on: req).filter(\.name == searchTerm).all()
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,40 +3,7 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    router.post("api", "restaurants") { req -> Future<Restaurant> in
-        return try req.content.decode(Restaurant.self).flatMap(to: Restaurant.self) { restaurant in
-            return restaurant.save(on: req)
-        }
-    }
     
-    router.get("api", "restaurants") { req -> Future<[Restaurant]> in
-        return Restaurant.query(on: req).all()
-    }
-    
-    router.get("api", "restaurants", Restaurant.parameter) { req -> Future<Restaurant> in
-        return try req.parameters.next(Restaurant.self)
-    }
-    
-    router.put("api", "restaurants", Restaurant.parameter) { req -> Future<Restaurant> in
-        return try flatMap(to: Restaurant.self, req.parameters.next(Restaurant.self), req.content.decode(Restaurant.self), { (restaurant, updatedRestaurant) in
-            restaurant.name = updatedRestaurant.name
-            return restaurant.save(on: req)
-        })
-    }
-    
-    router.delete("api", "restaurants", Restaurant.parameter) { req -> Future<HTTPStatus> in
-        return try req.parameters.next(Restaurant.self).delete(on: req).transform(to: HTTPStatus.noContent)
-    }
-    
-    router.get("api", "restaurants", "search") { req -> Future<[Restaurant]> in
-        guard let searchTerm = req.query[String.self, at: "term"] else {
-            throw Abort(.badRequest)
-        }
-        return Restaurant.query(on: req).filter(\.name == searchTerm).all()
-    }
+    let restaurantsController = RestaurantsController()
+    try router.register(collection: restaurantsController)
 }
